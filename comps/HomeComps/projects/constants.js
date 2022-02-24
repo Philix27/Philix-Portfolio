5af1168e8de03fda5146dcfee9d15b3c285ffeb0
Philix27 <philixbob@gmail.com>
2022-02-24 05:43:29 +0100

    Created and added link to project pages

--- comps/HomeComps/projects/constants.js
+++ comps/HomeComps/projects/constants.js
@@ -5,7 +5,7 @@ export const projects = [
   {
     title: 'Get2geda',
     group: "mobile",
-    detailsPage: "/project/mim",
+    detailsPage: "/project/g2g",
     githubLink: "",
     description: "An android app for Delta State University Students, (Nigeria). Contains an e-commerce section and a news section ",
     image: '/images/project/g2g.jpg',
@@ -29,7 +29,7 @@ export const projects = [
   {
     title: 'Rxedu - App',
     group: "mobile",
-    detailsPage: "/project/mim",
+    detailsPage: "/project/rxedu",
     description: "An Educaational App for Pharmacists preparing for various exams. Contains MCQs, and related materials need to ace pharmaceutical exams.",
       image: '/images/project/rxedu.jpg',
       tags: ['Dart', 'Flutter', 'Firebase', ],
@@ -40,7 +40,7 @@ export const projects = [
   },
   {
     title: 'Rxedu - Website',
-     detailsPage: "/project/mim",
+     detailsPage: "/project/rxeduweb",
     description: "An Educaational Website (For the app) for Pharmacists preparing for various exams. Contains MCQs, and related materials need to ace pharmaceutical exams.",
     group: "web",
       image: '/images/project/rxedu_web.jpg',
@@ -53,7 +53,7 @@ export const projects = [
   {
     title: 'Bioplus',
     group: "mobile",
-     detailsPage: "/project/mim",
+     detailsPage: "/project/bioplus",
     description: "A mobile app for selling and buying otc pharmaceuticals ",
     image: '/images/project/bioplus.jpg',
     tags: ['Dart', 'Flutter', 'Firebase', ],
@@ -65,7 +65,7 @@ export const projects = [
   {
     title: 'BTB',
     group: "mobile",
-     detailsPage: "/project/mim",
+     detailsPage: "/project/btb",
     description: "A Binance Cryptocurrency trading bot ",
     image: '/images/project/btb.jpg',
     tags: ['Dart', 'Python',  ],
@@ -77,7 +77,7 @@ export const projects = [
   {
     title: 'Portfolio',
     group: "web",
-     detailsPage: <Home/>,
+     detailsPage: '/',
     description: "My personal website for detailed explanation of all my projects.",
     image: '/images/project/portfolio.jpg',
     tags: ['Next.js',],
---
+++ pages/project/bioplus.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import Image from 'next/image';
+import styles from '../../comps/HomeComps/projects/details.module.css'
+import { projects } from '../../comps/HomeComps/projects/constants';
+import { Section, SectionTitle, SectionText } from '../../styles';
+import {   Img } from '../../comps/HomeComps/projects/projectsStyles';
+
+export default function Mim() {
+   
+    const { title, image, description, visit} = projects[4];
+    return (
+        <div className={styles.wrapper}>
+                <div>
+                <SectionTitle >{title}</SectionTitle>
+                </div>
+                <Image
+                layout="responsive"
+                objectFit="cover"
+                height={250}
+                width={1040}
+                src={image}
+            />  
+            {/* <div className={styles.imgBox}>
+                <Img src={image} />  
+            </div>   */}
+               
+            <div className={styles.description}>
+            <div className={styles.SectionText}>{description}</div>
+                <div className={styles.SectionText}>
+                     <ul>
+                    <a href={visit} target="_blank"><li>View Source Code on Github</li></a>        
+                    <a><li>View on Playstore - Android</li></a>        
+                          
+                </ul>
+            </div>
+                <div className={styles.SectionText}>
+                    This app consist of several pages which will be discussed in a bit.
+                <ul>
+                    <li>Dashboard</li>        
+                    <li>Daily Sales</li>        
+                    <li>Expensens</li>        
+                    <li>Products List</li>        
+                    <li>Summary</li>        
+                    <li>Notification Page</li>        
+                </ul>
+            </div>
+
+            </div>
+        
+         
+        </div>
+            
+    );
+}
+
+
+
+
+
