02904fc043a7d0b75b2b23bb7a4de032fc27173e
Philix27 <philixbob@gmail.com>
2022-10-08 03:25:00 +0100

    projects retrieved from md files working

---
+++ _projects/rxeduweb.md
@@ -0,0 +1,12 @@
+---
+title: "Rxedu - Website"
+summary: "Crisis associated with Africa"
+cover_image: "/images/project/rxedu_web.jpg"
+category: "Africa"
+group: "web"
+id: 1;
+---
+
+An Educaational App for Pharmacists preparing for various exams. Contains MCQs, and related materials need to ace pharmaceutical exams.
+
+Willo
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,17 +1,53 @@
-import ProjectsGroup from '../../comps/HomeComps/projects/projectsList'
+import ProjectsGroup from "../../comps/HomeComps/projects/allProjectsList";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { sortByDate } from "../../utils";
 
-
-export default function ProjectsPage() {
+export default function ProjectsPage({ projects }) {
   return (
-    < > 
-      <div className='section'>
-      <h1 className='sectionTitle'>Mobile Apps</h1>
-      </div>  
-      <ProjectsGroup category="mobile"/>
-      <div className='section'>
-      <h1 className='sectionTitle'>Web Apps</h1>
-      </div>  
-      <ProjectsGroup category="web"/>
+    <>
+      <div className="section">
+        <h1 className="sectionTitle">Flutter UI</h1>
+      </div>
+      <ProjectsGroup category="flutter-ui" projectCol={projects} />
+      <div className="section">
+        <h1 className="sectionTitle">Mobile Apps</h1>
+      </div>
+      <ProjectsGroup category="mobile" projectCol={projects} />
+      <div className="section">
+        <h1 className="sectionTitle">Web Apps</h1>
+      </div>
+      <ProjectsGroup category="web" projectCol={projects} />
     </>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  // Get files from the projects dir
+  const files = fs.readdirSync(path.join("_projects"));
+  // Get slug and frontmatter from projects
+  const books = files.map((filename) => {
+    // Create slug
+    const slug = filename.replace(".md", "");
+
+    // Get frontmatter
+    const markdownWithMeta = fs.readFileSync(
+      path.join("_projects", filename),
+      "utf-8"
+    );
+
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  return {
+    props: {
+      projects: books.sort(sortByDate),
+    },
+  };
 }
