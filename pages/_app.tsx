00bd9c245245c50f6669689cbf7771710efe9d85
Philix27 <philixbob@gmail.com>
2023-03-10 23:10:23 +0100

    deleted .nest

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,6 @@ import Layout from "../views/Layout";
 import "../styles/index.scss";
 import { type AppType } from "next/app";
 
-// function MyApp({ Component, pageProps }) {
-//   return (
-//     <Layout>
-//       <Component {...pageProps} />
-//     </Layout>
-//   );
-// }
-
-// export default MyApp;
-
 const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
   return (
     <Layout>
