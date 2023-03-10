ce5cffa15c3266528ee8f66279a1d7b4800e2e85
Philix27 <philixbob@gmail.com>
2023-03-10 23:06:21 +0100

    updated _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,23 @@
 import Layout from "../views/Layout";
 import "../styles/index.scss";
+import { type AppType } from "next/app";
 
-function MyApp({ Component, pageProps }) {
+// function MyApp({ Component, pageProps }) {
+//   return (
+//     <Layout>
+//       <Component {...pageProps} />
+//     </Layout>
+//   );
+// }
+
+// export default MyApp;
+
+const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
   );
-}
+};
 
 export default MyApp;
